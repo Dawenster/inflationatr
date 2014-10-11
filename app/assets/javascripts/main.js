3abdc6cd57abe9b4835a0f48cefe236bf55c98f6
David Wen <dstwen@gmail.com>
2014-10-10 23:32:46 -0700

    did it

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,55 @@
+if(typeof(String.prototype.trim) === "undefined")
+{
+    String.prototype.trim = function() 
+    {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
+}
+
 $(document).ready(function() {
+  var data = {}
+
   $.ajax({
     url: "/load",
     dataType: "json"
   })
-  .done(function() {
-    alert( "second success" );
+  .done(function(result) {
+    data = result.data;
   })
   .fail(function() {
     alert( "error" );
   });
+
+  $("#inflationate").click(function() {
+    var value = $("#input").val().trim();
+    var valueArr = value.split(" ");
+    var returnArr = []
+    
+    for (var i = 0; i < valueArr.length; i++) {
+      var wordWithPunc = valueArr[i].toUpperCase();
+
+      var wordWithPuncArr = wordWithPunc.split(/\s*\b\s*/);
+
+      var word = wordWithPuncArr.shift();
+
+      var translated = data[word];
+
+      var finalTranslated = ""
+
+      if (translated) {
+        finalTranslated = translated.toUpperCase()
+      } else {
+        finalTranslated = word
+      }
+
+      if (wordWithPuncArr.length > 0) {
+        finalTranslated += wordWithPuncArr.join("");
+      }
+
+      returnArr.push(finalTranslated);
+
+    };
+    $("#output").text(returnArr.join(" "));
+  })
+
 });
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,25 @@
+require 'csv'
+
 class WordsController < ApplicationController
   def load
     respond_to do |format|
-      file = File.open("#{Rails.root}/db/beep")
-      # binding.pry
+      data = {}
+      CSV.foreach("db/translations/combined.csv") do |row|
+        original = row.first
+        translated = row.last
+        data[original] = translated
+      end
       format.json { render :json => { :data => data } }
     end
   end
 end
+
+# CSV.open("db/first.csv", "wb") do |csv|
+#     output = row.join(" ").split(/\s+/m)
+#     word = output.shift
+#     output = output.join(" ")
+#     if output.include?("f er s t")
+#       csv << [word, output]
+#     end
+#   end
+# end
